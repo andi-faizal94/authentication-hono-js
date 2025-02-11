d3d5b0b5dc9b6b16cc1734ebb8e79e0d4695c4f5
andi faizal <andifaizal@MacBook-Pro-andi.local>
2025-02-11 08:31:09 +0700

    feat: add rate limit

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = new Hono();
 
 const rateLimiterUsingThirdParty = rateLimit({
   windowMs: 24 * 60 * 60 * 1000,
-  max: 100,
+  max: 2,
   message: "You have exceeded the 100 requests in 24 hrs limit!",
   standardHeaders: true,
   legacyHeaders: false,

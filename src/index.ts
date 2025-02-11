2a707e6e39d6fcebabe06aeb29e6157ffe3f4242
andi faizal <andifaizal@MacBook-Pro-andi.local>
2025-02-11 08:27:17 +0700

    feat: add rate limit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
 import { Routes } from "./routes";
+import rateLimit from "express-rate-limit";
 
 const prisma = new PrismaClient();
 const app = new Hono();
+
+const rateLimiterUsingThirdParty = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000,
+  max: 100,
+  message: "You have exceeded the 100 requests in 24 hrs limit!",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use("api", rateLimiterUsingThirdParty);
 
 app.route("/api", Routes);
 
